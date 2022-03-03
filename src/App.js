a9796ad1ad287876c8a60165b9361277d451e249
laevatein2070 <zenzoza007@gail.com>
2022-03-03 22:35:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  let [tlist, setTList] = useState([]);
+  let [tdate, setTDate] = useState([]);
+
+  let [subject, setSubject] = useState([]);
+  let [date, setDate] = useState([]);
+
+  function handleClear(e) {
+    e.preventDefault();
+    setTList([]);
+    setTDate([]);
+    console.log(tlist, tdate);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTList(tlist.concat(subject));
+    setTDate(tdate.concat(date));
+    console.log(tlist, tdate);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClear} className='Clear'>Clear</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To-Do List
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <form className='Add-List'>
+            <input 
+              className='Input'
+              placeholder='Subject'
+              onChange={(e) => setSubject(e.target.value)}
+            >
+            </input>
+            <input
+              className='Input'
+              placeholder='Due Date'
+              onChange={(e) => setDate(e.target.value)}
+            >
+            </input>
+            <button className='Button' onClick={handleSubmit}>&gt;</button>
+          </form>
+          <p>Result</p>
+          <div className='Result'>
+            <div>Subject{tlist}</div>
+            <div>Date{tdate}</div>
+          </div>
+        </div>
       </header>
     </div>
   );
