36eab9ab067f448dee509e4fc3f45558b0f9dc46
laevatein2070 <zenzoza007@gail.com>
2022-03-04 12:28:07 +0700

    update result display

--- src/App.js
+++ src/App.js
@@ -6,50 +6,81 @@ function App() {
   let [tlist, setTList] = useState([]);
   let [tdate, setTDate] = useState([]);
 
-  let [subject, setSubject] = useState([]);
-  let [date, setDate] = useState([]);
+  let [subject, setSubject] = useState(null);
+  let [date, setDate] = useState(null);
 
   function handleClear(e) {
     e.preventDefault();
     setTList([]);
     setTDate([]);
-    console.log(tlist, tdate);
+  }
+
+  function handleClearForm() {
+    document.getElementById("todoForm").reset();
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    setTList(tlist.concat(subject));
-    setTDate(tdate.concat(date));
-    console.log(tlist, tdate);
+    if (!subject || !date) {
+      alert('Pleast insert subject and date')
+    }
+    else if (date.match(/^[0-9]+$/) === null || date.length !== 8) {
+        alert('Pleast enter a valid date')
+      }
+    else {
+      const day = date.slice(0,2);
+      const month = date.slice(2,4);
+      const year = date.slice(4);
+
+      date = day + '-' + month + '-' + year;
+      setTList(tlist.concat(subject));
+      setTDate(tdate.concat(date));
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={handleClear} className='Clear'>Clear</button>
-        <p>
-          To-Do List
-        </p>
-        <div>
-          <form className='Add-List'>
+        
+        <div className='BodyDiv'>
+          
+          <form id="todoForm" className='Add-List' onSubmit={handleSubmit}>
+            <p>To-Do List</p>
             <input 
               className='Input'
               placeholder='Subject'
+              maxlength="30"
               onChange={(e) => setSubject(e.target.value)}
             >
             </input>
             <input
               className='Input'
-              placeholder='Due Date'
+              placeholder='Due Date (DDMMYYYY)'
+              maxlength="8"
               onChange={(e) => setDate(e.target.value)}
             >
             </input>
-            <button className='Button' onClick={handleSubmit}>&gt;</button>
+            <button className='Button' onClick={handleClearForm} type='submit'>&gt;</button>
           </form>
-          <p>Result</p>
-          <div className='Result'>
-            <div>Subject{tlist}</div>
-            <div>Date{tdate}</div>
+
+          <div className='ResultDiv'>
+            <div className='Result'>
+              <div className='ResultHeader'>{tlist.length > 0 ? "Subject": ''}</div>
+              <div className='ResultHeader'>{tlist.length > 0 ? "Date": ''}</div>
+            </div>
+            <div className='ResultList'>
+              <div className='ResultBox'>
+                {tlist.map((tlist) => 
+                  <p>{tlist}</p>)
+                }
+              </div>
+              <div className='ResultBox'>
+                {tdate.map((tdate) => 
+                  <p>{tdate}</p>)
+                }
+              </div>
+            </div>
           </div>
         </div>
       </header>
